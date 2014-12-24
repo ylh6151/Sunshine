d52489b960ef77c0cf8f75214d2b65dd256c3300
miyoo <miyoo@ebay.com>
2014-12-24 09:52:36 +0900

    no message

--- app/src/main/java/com/example/miyoo/sunshine/MainActivity.java
+++ app/src/main/java/com/example/miyoo/sunshine/MainActivity.java
@@ -1,25 +1,10 @@
 package com.example.miyoo.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -30,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container,new ForecastFragment())
                     .commit();
         }
     }
